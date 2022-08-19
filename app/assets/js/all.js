62a75a78fcfb2db18c59751f473c9149924f9ee2
waynechen <n0918679182@gmail.com>
2022-08-19 17:53:53 +0800

    新增首頁

---
+++ app/assets/js/all.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+//此處JS部分參考 WEI_RIO#9342 Day 39 每日
+var swiper = new Swiper(".share-Swiper", {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
