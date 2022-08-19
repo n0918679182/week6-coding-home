b11fda3a05d86ecb8360154c4a3be1aa86a591d9
waynechen <n0918679182@gmail.com>
2022-08-19 17:55:21 +0800

    Update 2022-08-19T09:55:16.509Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,15 @@
+"use strict";
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: "auto",
   spaceBetween: 30,
   grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+    clickable: true
+  }
+}); //此處JS部分參考 WEI_RIO#9342 Day 39 每日
 
-
-//此處JS部分參考 WEI_RIO#9342 Day 39 每日
 var swiper = new Swiper(".share-Swiper", {
   effect: 'fade',
   fadeEffect: {
@@ -17,9 +17,10 @@ var swiper = new Swiper(".share-Swiper", {
   },
   navigation: {
     nextEl: ".swiper-next",
-    prevEl: ".swiper-prev",
+    prevEl: ".swiper-prev"
   },
   pagination: {
-    el: ".swiper-pagination",
-  },
+    el: ".swiper-pagination"
+  }
 });
+//# sourceMappingURL=all.js.map
